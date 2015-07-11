ab7461b284c82c3afff129c088d016e4fbef84c9
Kim Jinwoo <kidjinwoo@me.com>
2015-07-11 23:24:16 +0900

    *alpha-120

--- alpha/backon.sh
+++ alpha/backon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ##############################################
-# BackOn alpha-119
+# BackOn alpha-120
 TOOL_BUILD_TYPE=alpha
-TOOL_BUILD_NUM=119
+TOOL_BUILD_NUM=120
 ##############################################
 
 function setEnglish(){
@@ -581,7 +581,6 @@ function ClearKey(){
 }
 
 function checkRoot(){
-	echo "${command1}"
 	if [[ ! "${command1}" == "--skip-checkRoot" ]]; then
 		if [ "$(id -u)" != "0" ]; then
 			applyRed
