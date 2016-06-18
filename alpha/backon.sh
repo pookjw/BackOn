36789a3feb3e6370072e8ff8259e1c9c971031de
Kim Jinwoo <kidjinwoo@me.com>
2016-06-19 02:27:33 +0900

    *alpha-338

--- alpha/backon.sh
+++ alpha/backon.sh
@@ -4,9 +4,9 @@
 # kidjinwoo@me.com
 # GitHub : https://github.com/pookjw
 ##############################################
-# BackOn alpha-337-official
+# BackOn alpha-338-official
 TOOL_BUILD_TYPE=alpha
-TOOL_BUILD_NUM=337
+TOOL_BUILD_NUM=338
 TOOL_RELEASE=official
 # If you're planning to create unofficial build, please change TOOL_RELEASE value.
 ##############################################
@@ -2359,6 +2359,9 @@ function rebootDevice(){
 
 function customBackup(){
 	if [[ -d "/tmp/BackOn/Backup/${BACKUP_NAME}" && ! -z "${BACKUP_NAME}" ]]; then
+		if [[ -f "/tmp/BackOn/Backup/${BACKUP_NAME}/Custom" ]]; then
+			rm "/tmp/BackOn/Backup/${BACKUP_NAME}/Custom"
+		fi
 		if [[ ! -d "/tmp/BackOn/Backup/${BACKUP_NAME}/Custom" ]]; then
 			mkdir -p "/tmp/BackOn/Backup/${BACKUP_NAME}/Custom"
 		fi
